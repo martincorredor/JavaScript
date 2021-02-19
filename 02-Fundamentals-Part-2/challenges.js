38cd7211ad9047e0fb075137162e1ed41ab3a163
martincorredor <dev.martincorredor@gmail.com>
2021-02-19 17:22:09 -0500

    Add challenge #1

--- 02-Fundamentals-Part-2/challenges.js
+++ 02-Fundamentals-Part-2/challenges.js
@@ -1,4 +1,6 @@
-
+'use strict'
+/*
+console.log("Hola mundo");
 
 //Coding challenge #1
 function calcAverage(score1, score2, score3){
@@ -6,23 +8,49 @@ function calcAverage(score1, score2, score3){
 }
 
 
-function checkWinner(avgDolphins, avgKoalas){
-    if(avgDolphins > (avgKoalas/2)){
-        console.log `Dolphins win ${avgDolphins} vs. ${avgKoalas}`;
+function checkWinner(avgDolph, avgKoalas){
+    if(avgDolph > (avgKoalas/2)){
+        console.log `Dolphins win ${avgDolph} vs. ${avgKoalas}`;
     }
-    else if(avgKoalas > (avgDolphins/2)){
-        console.log `Koalas win ${avgKoalas} vs. ${avgDolphins}`;
+    else if(avgKoalas > (avgDolph/2)){
+        console.log `Koalas win ${avgKoalas} vs. ${avgDolph}`;
     }
     else{
         "There is not a winner";
     }
 }
 console.log('Data 1: Dolphins score 44, 23, 71. Koalas score 65, 54, 49');
-let avgDolphins1 = calcAverage(44, 23, 71);
+let avgDolph1 = calcAverage(44, 23, 71);
+console.log(avgDolph1);
 let avgKoalas1 = calcAverage(65, 54, 49);
-checkWinner(avgDolphins1, avgKoalas1);
+console.log(avgKoalas1);
+
+checkWinner(avgDolph1, avgKoalas1);
 
 console.log('Data 1: Dolphins score 85, 54, 41. Koalas score 23, 34, 27');
-avgDolphins2 = calcAverage(81, 54, 41);
-avgKoalas2 = calcAverage(23, 34, 27);
-checkWinner(avgDolphins2, avgKoalas2);
+let avgDolph2 = calcAverage(81, 54, 41);
+let avgKoalas2 = calcAverage(23, 34, 27);
+checkWinner(avgDolph2, avgKoalas2);
+*/
+
+//Coding challenge #1
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log(calcAverage(3, 4, 5));
+
+//Test1
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = function(avgDolph, avgKoalas)
+{
+  if (avgDolph >= 2 * avgKoalas) {
+		console.log(`Dolphins win (${avgDolph}) vs (${avgKoalas})`);
+  }
+	else if (avgKoalas >= 2 * avgDolph) {
+		console.log(`Koalas win (${avgKoalas}) vs (${avgDolph})`);
+	} else {
+		console.log('No team wins');
+	}
+}
+checkWinner(scoreDolphins, scoreKoalas);
